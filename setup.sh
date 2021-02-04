eb66be92874b24d7a45fbf7bb16a1578971ef0e6
sgah <sgah@42.student.fr>
2021-02-04 20:48:05 +0100

    grafana_setpup

--- setup.sh
+++ setup.sh
@@ -16,18 +16,14 @@ minikube start --driver=docker
 eval $(minikube docker-env)
 
 # Build docker images.
-# ""> /dev/null 2>&1" redirects the output of your program to /dev/null. Include both the Standard Error and Standard Out
-# ">" is for redirect. "/dev/null" is a black hole where any data sent, will be discarded
-# "2" is the file descriptor for Standard Error. "1" is the file descriptor for Standard Out.
-# "&" is the symbol for file descriptor (without it, the following 1 would be considered a filename).
 echo "${GREEN}Docker build init${END}"
-docker build -t my_nginx src/nginx > /dev/null 2>&1
-docker build -t my_wordpress src/wordpress > /dev/null 2>&1
-docker build -t my_mysql src/mysql > /dev/null 2>&1
-docker build -t my_phpmyadmin src/phpmyadmin > /dev/null 2>&1
-docker build -t my_ftps src/ftps > /dev/null 2>&1
-#docker build -t my_grafana src/grafana > /dev/null 2>&1
-#docker build -t my_influxdb src/influxdb > /dev/null 2>&1
+docker build -t my_nginx src/nginx 
+docker build -t my_wordpress src/wordpress 
+docker build -t my_mysql src/mysql 
+docker build -t my_phpmyadmin src/phpmyadmin 
+docker build -t my_ftps src/ftps 
+docker build -t my_grafana src/grafana 
+#docker build -t my_influxdb src/influxdb 
 echo "${BLUE}Docker build completed${END}"
 
 # Apply yaml resources.
@@ -40,7 +36,7 @@ kubectl apply -f src/mysql/mysql.yaml
 kubectl apply -f src/ftps/ftps.yaml
 kubectl apply -f src/phpmyadmin/phpmyadmin.yaml
 kubectl apply -f src/wordpress/wordpress.yaml
-#kubectl apply -f src/grafana/grafana.yaml
+kubectl apply -f src/grafana/grafana.yaml
 #kubectl apply -f src/influxdb/influxdb.yaml
 echo "${BLUE}Deploy completed${END}"
 
