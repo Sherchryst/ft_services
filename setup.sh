7d6f084b3e39be76811b5d3bdc0cd093d2ec5e6f
sgah <sgah@42.student.fr>
2021-02-05 18:57:47 +0100

    almost

--- setup.sh
+++ setup.sh
@@ -10,7 +10,7 @@ minikube delete
 killall -TERM kubectl minikube VBoxHeadless
 
 # Start minikube.
-minikube start --driver=docker
+minikube --vm-driver=docker start
 
 # Use the docker daemon from minikube.
 eval $(minikube docker-env)
@@ -21,26 +21,26 @@ printf "Minikube IP: ${IP}"
 # Build docker images.
 echo "${GREEN}Docker build init${END}"
 docker build -t my_nginx src/nginx 
-docker build -t my_wordpress src/wordpress 
-docker build -t my_mysql src/mysql 
-docker build -t my_phpmyadmin src/phpmyadmin 
-docker build -t my_ftps src/ftps 
+docker build -t my_wordpress src/wordpress --build-arg IP=${IP}
+docker build -t my_mysql src/mysql --build-arg IP=${IP}
+docker build -t my_phpmyadmin src/phpmyadmin --build-arg IP=${IP}
+docker build -t my_ftps src/ftps --build-arg IP=${IP}
 docker build -t my_grafana src/grafana 
 docker build -t my_influxdb src/influxdb 
 echo "${BLUE}Docker build completed${END}"
 
-# Apply yaml resources.
+# create yaml resources.
 echo "${GREEN}Deploy init${END}"
 kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.9.3/manifests/namespace.yaml
 kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.9.3/manifests/metallb.yaml
 kubectl apply -f src/metallb/metallb.yaml
-kubectl apply -f src/nginx/nginx.yaml
-kubectl apply -f src/mysql/mysql.yaml
-kubectl apply -f src/ftps/ftps.yaml
-kubectl apply -f src/phpmyadmin/phpmyadmin.yaml
-kubectl apply -f src/wordpress/wordpress.yaml
-kubectl apply -f src/grafana/grafana.yaml
-kubectl apply -f src/influxdb/influxdb.yaml
+kubectl create -f src/nginx/nginx.yaml
+kubectl create -f src/mysql/mysql.yaml
+kubectl create -f src/ftps/ftps.yaml
+kubectl create -f src/phpmyadmin/phpmyadmin.yaml
+kubectl create -f src/wordpress/wordpress.yaml
+kubectl create -f src/grafana/grafana.yaml
+kubectl create -f src/influxdb/influxdb.yaml
 echo "${BLUE}Deploy completed${END}"
 
 # Setup metalLB secret.
@@ -50,6 +50,10 @@ kubectl create secret generic -n metallb-system memberlist  --from-literal=secre
 minikube addons enable dashboard
 minikube addons enable ingress
 minikube addons enable metrics-server
+
+
+echo "Opening the network in your browser"
+xdg-open http://$IP
 
 # Open dashboard.
 minikube dashboard
